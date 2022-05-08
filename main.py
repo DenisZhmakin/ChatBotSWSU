a7e3b667710da354966c3c36d75cbc9f1d873879
xdragon86 <den.zhmakin.00@gmail.com>
2022-05-08 17:19:23 +0300

    Добавил параллельный запуск чат-ботов

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from threading import Thread
+
 from dotenv import load_dotenv
 
 from chatbots.telegram import TelegramBot
@@ -8,11 +10,14 @@ from chatbots.vkontakte import VkontakteBot
 def _main():
     load_dotenv()
 
-    # vkbot = VkontakteBot(os.getenv("VK_TOKEN"))
+    vkbot = VkontakteBot(os.getenv("VK_TOKEN"))
     tgbot = TelegramBot(os.getenv("TG_TOKEN"))
 
-    # vkbot.run_bot()
-    tgbot.run_bot()
+    t1 = Thread(target=vkbot.run_bot)
+    t2 = Thread(target=tgbot.run_bot)
+
+    t1.start()
+    t2.start()
 
 
 if __name__ == '__main__':
